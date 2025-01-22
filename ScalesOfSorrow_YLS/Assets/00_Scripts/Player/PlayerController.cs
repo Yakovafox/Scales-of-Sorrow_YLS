b3ffb6f59c042731099f617d4e8dee4703504627
Boto2512 <71253479+Boto2512@users.noreply.github.com>
2025-01-22 09:52:15 +0000

    Player Dash
    - Input map for dash
    - Update movement controller for dashes
    - Updated player prefab values

--- ScalesOfSorrow_YLS/Assets/00_Scripts/Player/PlayerController.cs
+++ ScalesOfSorrow_YLS/Assets/00_Scripts/Player/PlayerController.cs
@@ -13,21 +13,32 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float pSpeed;
     [SerializeField] private Vector2 movementInput = Vector2.zero;
     private Transform pTransform;
+    private Rigidbody pRB;
     private float pScale;
+
+    [Header("Dash")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashCooldown;
+    private bool canDash;
+    private bool isDash;
+
     #endregion
     void Start()
     {
         pTransform = transform;
+        pRB = GetComponent<Rigidbody>();
         pScale = pTransform.localScale.x;
     }
 
     void Update()
     {
-        
+        if (isDash) { return; }
     }
 
     private void FixedUpdate()
     {
+        if (isDash) { return; }
        MoveInput();
     }
 
@@ -37,6 +48,10 @@ public class PlayerController : MonoBehaviour
     {
         movementInput = context.ReadValue<Vector2>();
     }
+    public void OnDash(InputAction.CallbackContext context) 
+    {
+        StartCoroutine(Dash());
+    }
 
 
     void MoveInput()
@@ -44,8 +59,24 @@ public class PlayerController : MonoBehaviour
         Vector3 axis = new Vector3(movementInput.x, 0, movementInput.y);
         pTransform.position += (axis.normalized * pSpeed * Time.deltaTime);
 
+
         if(axis.x > 0) { pTransform.localScale = new Vector3(pScale, pTransform.localScale.y, pTransform.localScale.z); }
         else if (axis.x < 0) { pTransform.localScale = new Vector3(-pScale, pTransform.localScale.y, pTransform.localScale.z); }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDash = true;
+
+        pRB.velocity = new Vector3(movementInput.x, 0, movementInput.y) * dashSpeed;
+
+        yield return new WaitForSeconds(dashTime);
+        isDash = false;
+        pRB.velocity = new Vector3(0, 0, 0);
+
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 
     #endregion
