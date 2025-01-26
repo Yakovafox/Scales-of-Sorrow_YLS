bb979c188637a408d7d179655501fd5e31c542c3
Boto2512 <71253479+Boto2512@users.noreply.github.com>
2025-01-26 14:12:09 +0000

    Player Attack Logic in
    -currently uses right thumbstick and right trigger to attack
    - bug - Minor Issue | with box cast not accurately rotating to attack direction

--- ScalesOfSorrow_YLS/Assets/00_Scripts/Player/PlayerController.cs
+++ ScalesOfSorrow_YLS/Assets/00_Scripts/Player/PlayerController.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using RotaryHeart.Lib.PhysicsExtension;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Physics = RotaryHeart.Lib.PhysicsExtension.Physics;
 
 public class PlayerController : MonoBehaviour
 {
@@ -14,7 +18,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Vector2 movementInput = Vector2.zero;
     private Transform pTransform;
     private Rigidbody pRB;
-    private float pScale;
+    [SerializeField] private SpriteRenderer pSR;
 
     [Header("Dash")]
     [SerializeField] private float dashSpeed;
@@ -23,12 +27,26 @@ public class PlayerController : MonoBehaviour
     private bool canDash;
     private bool isDash;
 
+    [Header("Attack")]
+    [SerializeField] private Vector2 attackDirection = Vector2.zero;
+    [SerializeField] private LayerMask attackMask;
+    [SerializeField] private bool canAttack;
+    [SerializeField] private float attackRange;
+    [SerializeField] private float attackSize;
+    [SerializeField] private float aimSpeed;
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float attackDamage;
+
+    [Header("Upgrades")]
+    [SerializeField] private bool upgradeDash;
+    [SerializeField] private bool upgradeShield;
+    [SerializeField] private bool upgradeXXX;
+
     #endregion
     void Start()
     {
         pTransform = transform;
         pRB = GetComponent<Rigidbody>();
-        pScale = pTransform.localScale.x;
     }
 
     void Update()
@@ -39,7 +57,7 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         if (isDash) { return; }
-       MoveInput();
+        MoveInput();
     }
 
     #region Movement
@@ -48,23 +66,21 @@ public class PlayerController : MonoBehaviour
     {
         movementInput = context.ReadValue<Vector2>();
     }
-    public void OnDash(InputAction.CallbackContext context) 
-    {
-        StartCoroutine(Dash());
-    }
-
-
-    void MoveInput()
+    
+    private void MoveInput()
     {
         Vector3 axis = new Vector3(movementInput.x, 0, movementInput.y);
         pTransform.position += (axis.normalized * pSpeed * Time.deltaTime);
 
-
-        if(axis.x > 0) { pTransform.localScale = new Vector3(pScale, pTransform.localScale.y, pTransform.localScale.z); }
-        else if (axis.x < 0) { pTransform.localScale = new Vector3(-pScale, pTransform.localScale.y, pTransform.localScale.z); }
+        if(axis.x > 0) { pSR.flipX = false; }
+        else if (axis.x < 0) {pSR.flipX = true; }
+    }
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (upgradeDash) { return; } else if (context.started) { StartCoroutine(DefaultDash()); }
     }
 
-    private IEnumerator Dash()
+    private IEnumerator DefaultDash()
     {
         canDash = false;
         isDash = true;
@@ -78,6 +94,44 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
     }
+    #endregion
+
+    #region Attack
+
+    public void OnAim(InputAction.CallbackContext context)
+    {
+        attackDirection = context.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (canAttack) { StartCoroutine(Attack()); }
+        Debug.Log("Attack");
+    }
+
+    private IEnumerator Attack()
+    {
+        canAttack = false;
+
+        Vector3 direction = new Vector3(attackDirection.x, 0, attackDirection.y);
+        Vector3 rotateDirection = (new Vector3(direction.x - transform.position.x, 0, direction.z - transform.position.z)).normalized;
+        float angle = Mathf.Atan2(rotateDirection.x, rotateDirection.z) * Mathf.Rad2Deg;
+
+        Vector3 rotate = new Vector3(0, angle, 0);
+        RaycastHit[] hits = Physics.BoxCastAll(pTransform.position, new Vector3(attackSize, attackSize, attackSize), direction, quaternion.Euler(rotate), attackRange, attackMask, PreviewCondition.Both, 1f, Color.green, Color.red);
+
+        //play particle effect
+        //SoundEffect
+
+        for (int i = 0; i < hits.Length; i++) 
+        {
+            //Damage Enemies
+        }
+
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
+    }
 
     #endregion
+
 }
