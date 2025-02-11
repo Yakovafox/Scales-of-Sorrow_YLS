c6ce824c1f888c1ea5fe508a26315048bb471799
Boto2512 <71253479+Boto2512@users.noreply.github.com>
2025-02-11 18:01:18 +0000

    Started implementing controls for main menu

--- ScalesOfSorrow_YLS/Assets/00_Scripts/04_Menus&SceneTranstition/MainMenu.cs
+++ ScalesOfSorrow_YLS/Assets/00_Scripts/04_Menus&SceneTranstition/MainMenu.cs
@@ -7,21 +7,28 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayButton()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        Debug.Log("LoadScene");
     }
 
     public void SettingsButton()
     {
+
+        Debug.Log("Shwo Settings");
         //load or show settings menu
     }
 
     public void CreditsButton()
     {
+
+        Debug.Log("SHow Credits");
         //load or show credits menu
     }
 
     public void QuitButton()
     {
         Application.Quit();
+        Debug.Log("Quit Game");
     }
 }
